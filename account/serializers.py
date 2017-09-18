5bda4affd7899509a2b852556b05af979e75af75
ajith <ajith.kjames3@gmail.com>
2017-09-18 17:58:42 +0530

    user update and password change fix

--- account/serializers.py
+++ account/serializers.py
@@ -19,6 +19,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username','password','first_name','empid','age','teamlead','skills','team_set','project_set')
+        extra_kwargs = {
+            'password': {'write_only': True, 'required':False}
+        }
 
     def create(self, validated_data):
         skills_data = validated_data.pop('skills')
@@ -29,3 +32,17 @@ class UserSerializer(serializers.ModelSerializer):
             skill = Skill.objects.create(**skill_data)
             user.skills.add(skill)
         return user
+
+    def update(self, instance, validated_data):
+        if validated_data.get('password'):
+            instance.set_password(validated_data.get('password'))
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.age = validated_data.get('age', instance.age)
+        instance.teamlead = validated_data.get('teamlead', instance.teamlead)
+        skills_data = validated_data.pop('skills')
+        instance.skills.clear()
+        for skill_data in skills_data:
+            skill = Skill.objects.create(**skill_data)
+            instance.skills.add(skill)
+        instance.save()
+        return instance
